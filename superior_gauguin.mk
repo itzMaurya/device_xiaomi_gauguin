250bc427e79460113d808a0584aba982e97f1fee
itzMaurya <mauryapradip04@gmail.com>
2023-04-18 21:09:42 +0530

    superior os bringup

--- superior_gauguin.mk
+++ superior_gauguin.mk
@@ -11,8 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from gauguin device
 $(call inherit-product, device/xiaomi/gauguin/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common superio os stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
+
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := superior_gauguin
